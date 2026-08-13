41a4ee6ace46f7ab7e305ca0896599049ff985c1
Badal Singh <badalrkt23@gmail.com>
2026-08-13 21:43:28 +0530

    added Experience Api for Experience secion

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors  from "cors";
+// import { Experience } from './utilities/index';
+
+const Experience = [
+    {
+    Id: 1,
+    CompanyName: "Suretech InfoSoft Pvt. Ltd.",
+    Post: "Intern",
+    Duration: "Oct 2024 – Mar 2025"
+  },
+  {
+    Id: 2,
+    CompanyName: "Suretech InfoSoft Pvt. Ltd.",
+    Post: "Software Engineer",
+    Duration: "Mar 2025 – Now"
+  }
+];
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// API routes
+
+app.get("/api/getExperience", (req, res) => {
+    res.status(200).json({
+        "status" : "Success",
+        "data"   : Experience,
+    });
+})
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`The server is running at : http://localhost:${PORT}`));
---
+++ Backend/utilities/index.ts
@@ -0,0 +1,14 @@
+export const Experience = [
+    {
+    Id: 1,
+    CompanyName: "Suretech InfoSoft Pvt. Ltd.",
+    Post: "Intern",
+    Duration: "Oct 2024 – Mar 2025"
+  },
+  {
+    Id: 2,
+    CompanyName: "Suretech InfoSoft Pvt. Ltd.",
+    Post: "Software Engineer",
+    Duration: "Mar 2025 – Now"
+  }
+];
