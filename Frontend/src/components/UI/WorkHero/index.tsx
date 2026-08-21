c8bf14137882421faf20bd507865dbf18439ae59
Badal Singh <badalrkt23@gmail.com>
2026-08-21 10:43:15 +0530

    Added two more videos for work section.

--- Frontend/src/components/UI/WorkHero/index.tsx
+++ Frontend/src/components/UI/WorkHero/index.tsx
@@ -3,6 +3,11 @@ import style from "./style"
 import SortingGraph from "../SortingGraph";
 
 const WorkHero = () => {
+
+  const videoFileArray = ["laptop", "web-developer", "work-hero"];
+  const randomInt = Math.random();
+  const videoSourceFile = Math.floor(randomInt * 3);
+
   return (
     <div className="flex flex-col md:flex-row gap-4 lg:gap-16 bg-[#ccd5ae] rounded-2xl w-full p-8">
       <div className="flex flex-col gap-4 lg:gap-8 text-sm">
@@ -15,8 +20,7 @@ const WorkHero = () => {
       </div>
       <div className="flex min-w-[230px] h-80 overflow-hidden rounded-[30%_70%_60%_40%/30%_30%_70%_70%] bg-white shadow-xl/30 bg-linear-to-r from-blud-600 to-white">
         {/* <SortingGraph/> */}
-        {/* <video muted autoPlay loop playsInline src="/web-developer.mp4"/> */}
-        <video muted autoPlay loop playsInline src="/laptop.mp4"/>
+        <video muted autoPlay loop playsInline src={`/${videoFileArray[videoSourceFile]}.mp4`}/>
       </div>
     </div>
   );
