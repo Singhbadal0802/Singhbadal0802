b382b6e43d82292a1a61f05f5cda033a3263f426
Badal Singh <badalrkt23@gmail.com>
2026-09-08 07:54:57 +0530

    Added fallback value of mongo URI

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI ?? 'mongodb+srv://badalrkt23_db_user:0sQmVJ5jKzYRfncQ@cluster0.abwxkqt.mongodb.net/users');
         console.log('✅ MongoDB connection successful')
     }catch(error){
         console.error('Error during mongoose connection : ',error.message);
