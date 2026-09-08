f8d3e14fed80b97899815fc48d542632c1efd589
Badal Singh <badalrkt23@gmail.com>
2026-09-08 08:00:08 +0530

    Added console to debug the URI issue.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI ?? 'mongodb+srv://badalrkt23_db_user:0sQmVJ5jKzYRfncQ@cluster0.abwxkqt.mongodb.net/users');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ MongoDB connection successful')
     }catch(error){
+        console.log('Fetched URI : ', process.env.MONGO_URI)
         console.error('Error during mongoose connection : ',error.message);
         process.exit(1);
     }
